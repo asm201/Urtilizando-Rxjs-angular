7aaa1b44d087756cc736fcb51993b808e9e31493
asm201 <asirikaku2000@gmail.com>
2023-08-01 10:27:57 -0300

    criação de pipeline com rxjs

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,18 +1,23 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LivroService {
 
-  private readonly API = "https://www.googleapis.com/books/v1/volumes?q=search+terms"
+  private readonly API = "https://www.googleapis.com/books/v1/volumes"
 
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<any>{
+  buscar(valorDigitado: string): Observable<Item[]>{
     const params = new HttpParams().append('q', valorDigitado)
-    return this.http.get(this.API, {params})
+    return this.http.get<LivrosResultado>(this.API, {params}).pipe(
+      tap((retornoAPI) => console.log('fluxo antes do map',retornoAPI)),
+      map(resultado => resultado.items),
+      tap((retornoAPI) => console.log('fluxo depois do map',retornoAPI)),
+    )
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,24 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subscription, debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs';
+import { Item, Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent{
 
-  listaLivros: [];
-  campoBusca: string = ''
+  //listaLivros: Livro[];
+  campoBusca = new FormControl()
+  //subscription: Subscription
+  //livro: Livro
 
   constructor(private service: LivroService) { }
 
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map((items) => this.livrosResultadoParaLivros(items))
+  )
+
+/*
   buscarLivros(){
-    this.service.buscar(this.campoBusca).subscribe(
-      (retornoAPI) => console.log(retornoAPI),
-      (error) => console.log(error)
-      )
+    this.subscription = this.service.buscar(this.campoBusca).subscribe(
+      {
+        next: (items) => {
+          this.listaLivros = this.livrosResultadoParaLivros(items)
+        },
+        error: erro => console.error(erro),
+      }
+    )
+  }*/
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[]{
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
   }
+
+  /*
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }*/
 
 }
 
